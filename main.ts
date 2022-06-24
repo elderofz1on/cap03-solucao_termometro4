23a3c917eb7e212e8033d49569834c73d35bc539
Wagner Rodrigues <wprodriguesbr@gmail.com>
2022-06-24 16:43:10 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+basic.forever(function () {
+    if (input.temperature() < 18) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (input.temperature() > 24) {
+        basic.showLeds(`
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    }
+})
